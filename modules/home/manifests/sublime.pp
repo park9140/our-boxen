3e2544316d4aaf237ce0460e324cf35ae791b216
Jonathan Park <jpark@home.com>
2013-09-18 22:14:18 -0700

    Adding sublime configuration files

--- modules/home/manifests/sublime.pp
+++ modules/home/manifests/sublime.pp
@@ -9,5 +9,19 @@ class home::sublime {
   sublime_text_3::package { 'Floobits':
     source => 'Floobits/floobits-sublime'
   }
-
+  sublime_text_3::package { 'Wombat Theme':
+    source => 'sheerun/sublime-wombat-theme'
+  }
+  sublime_text_3::package { 'BracketHighlighter':
+    source => 'facelessuser/BracketHighlighter'
+  }
+  sublime_text_3::package { 'sublime-jsdocs':
+    source => 'spadgos/sublime-jsdocs'
+  }
+  sublime_text_3::package { 'emmet-sublime':
+    source => 'sergeche/emmet-sublime'
+  }
+  sublime_text_3::package { 'ts3':
+    source => 'Railk/T3S'
+  }
 }
--- modules/people/manifests/park9140.pp
+++ modules/people/manifests/park9140.pp
@@ -1,17 +1,17 @@
 class people::park9140 {
   include home::fishShell
-  
+
   include home::sublime
-  
+
   include chrome
   include chrome::canary
 
   include iterm2::dev
-  
+
   include projects::ppmspa
 
   $home = "/Users/${::boxen_user}"
-  
+
   file { "${home}/.config/fish/personal.fish":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/park9140/personal.fish",
@@ -25,6 +25,21 @@ class people::park9140 {
     value  => 'Jonathan Park'
   }
 
+  repository{
+    'my sublime config':
+      source   => 'park9140/sublimeconfig', #short hand for github repos
+      provider => 'git',
+      path => "${home}/src/sublimeconfig",
+      force => true
+  }
+  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings":
+    ensure  => link,
+    target  => "${home}/src/sublimeconfig/Preferences.sublime-settings"
+  }
+  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings":
+    ensure  => link,
+    target  => "${home}/src/sublimeconfig/SublimeLinter.sublime-settings"
+  }
   file { "${home}/.bash_profile":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/park9140/.bash_profile"
